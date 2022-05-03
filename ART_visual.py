a0dd8edcacdaea56b24e6382f487345ee7646748
HANNAHBIBBY <hannah@bibbynet.co.uk>
2022-05-03 16:22:58 -0400

    Testing

--- ART_visual.py
+++ ART_visual.py
@@ -7,31 +7,47 @@ from vega_datasets import data
 @st.cache
 def load_data():
 
-    # Loading ART treatment rate data
-
-    art_data = pd.read_csv('data/ART_treatment_pct.csv', index_col = 0)
-    periods = art_data.columns
-    art_data.reset_index(inplace=True)
-    art_data = art_data.rename(columns = {'index':'Country'})
+    # Loading ART treatment rate data (saved in pct.csv file)
     
-    # Converting data into long format
-
-    art_df = art_data.melt(id_vars='Country', value_vars = periods, var_name = 'year', value_name = 'ART_rate')
-    art_df['metric'] = 'ART_rate'
-
-    #Replacing <1 and >98 to 1 and 98 respectively
+    art_rate = pd.read_csv('data/ART_treatment_pct.csv', index_col = 0)
+    periods = art_rate.columns
     
-    art_df['year'] = art_df['year'].replace(['<1'],0.9)
-    art_df['year'] = art_df['year'].replace(['>98'],98.1)
-    art_df['year'] = art_df['year'].replace(['...'],0)
+    # Converting treatment rate data into long format
 
-    return art_df
+    art_rate.reset_index(inplace=True)
+    art_rate = art_rate.rename(columns = {'index':'Country'})
+    art_rate = art_rate.melt(id_vars='Country', value_vars = periods, var_name = 'year', value_name = 'rate')
+    art_rate['metric'] = 'ART_rate'
+
+    #Replacing <1 and >98 to 0.5 and 98.5 respectively
+    
+    art_rate['rate'] = art_rate['rate'].replace(['<1'],0.5)
+    art_rate['rate'] = art_rate['rate'].replace(['>98'],98.5)
+    art_rate = art_rate.replace('...', 0)
+
+    # Ensuring columns are int
+    #art_rate['year'] = art_rate['year'].astype('int')
+    #art_rate['ART_rate'] = art_rate['ART_rate'].astype('int')
+
+    # Loading ART total treated population data (saved in no.csv file) UPDATE, NOT CURRENTLY LINKING TO CORRECT FILE
+    art_total = pd.read_csv('data/ART_treatment_pop.csv', index_col = 0)
+    art_total = art_total.replace('...', 0)
+    
+    return art_rate, art_total
 
 # Loading the ART treatment data
-art_df= load_data()
+art_rate, art_total  = load_data()
 
 #Check line for df - TO REMOVE FROM APP IN  FINAL VERSION
-st.write(art_df.head(5))
+st.write(art_rate.head(20))
+st.write(art_total.head(5))
+# st.write(art_tot.head5)
+
+# Creating table of percetange change per year in treated population
+
+pct_change = art_total.pct_change(axis = 1)
+
+st.write(pct_change)
 
 
 #Adding preliminary title
