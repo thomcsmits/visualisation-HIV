01c0b4aebaa59644711c1f343a226c59f9ba5470
HANNAHBIBBY <hannah@bibbynet.co.uk>
2022-05-03 22:34:14 -0400

    Added colour buckets

--- temporal_visual.py
+++ temporal_visual.py
@@ -9,6 +9,12 @@ from vega_datasets import data
 from data_prep import export_hiv
 hiv_df_long = export_hiv()
 
+bins = [-1,0,10000, 50000,100000, 150000, 200000]
+labels = ["-1-0","0 - 10000","10000-50000","50000-100000","100000-150000","150000-200000"]
+hiv_df_long['cases_bucket'] = pd.cut(hiv_df_long['Cases'], bins=bins, labels=labels)
+
+st.write(hiv_df_long.head())
+
 ## Allow using rows more than 5000
 alt.data_transformers.disable_max_rows(); 
 
@@ -38,6 +44,7 @@ background = alt.Chart(source
 #     fields = ['Country']
 #     )
 
+
 chart_base = alt.Chart(source
     ).properties( 
         width=width,
@@ -46,15 +53,21 @@ chart_base = alt.Chart(source
     # ).add_selection(selector
     ).transform_lookup(
         lookup='id',
-        from_=alt.LookupData(hiv_df_long_year, 'country-code', ['Country','Year','Cases']),
+        from_=alt.LookupData(hiv_df_long_year, 'country-code', ['Country','Year','cases_bucket']),
     )
 
+#cases_scale = alt.Scale(domain=[hiv_df_long['Cases'].min(), hiv_df_long['Cases'].max()])
 
-cases_scale = alt.Scale(domain=[hiv_df_long['Cases'].min(), hiv_df_long['Cases'].max()])
-cases_color = alt.Color(field="Cases", type="quantitative", scale=cases_scale)
+domain = ["No Data", "0 - 10k","10k-50k","50k-100k","100k-150k","150k +" ]
+range_ = ['grey', '#aec7e8', '#ffbb78', '#ff9896', 'yellow', 'green']
+range2 = alt.Scale(scheme='dark2')
+
+st.write('Testing3', bins)
+
+cases_color = alt.Color(field="Prevalence brackets", scale=alt.Scale(domain=domain,range=range_))
 chart_cases = chart_base.mark_geoshape().encode(
     color = cases_color,
-    tooltip = ['Country:N', 'Cases:Q']
+    tooltip = ['Country:N', 'cases_bucket:Q']
     #).transform_filter(
     #selector
     ).properties(
